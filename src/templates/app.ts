ca051351649568144e47ebe2f9f432b7d465238c
Nii Nartey <nii.tetteh@amalitech.com>
2025-03-25 16:55:08 +0000

    feat: migrate error handling to app.ts and implement TypeScript for routing

--- src/templates/app.ts
+++ src/templates/app.ts
@@ -1,27 +1,35 @@
-//GLOBAL MODULES
-const express = require('express');
-const morgan = require('morgan');
-require('dotenv').config();
+import express, { Request, Response, NextFunction } from 'express';
+import createError from 'http-errors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import apiRouter from './routes/api.route';
 
-//LOCAL MODULES
-const { apiDocsPage } = require('./routes/api.route');
-const errorRoute = require('../../routes/errors.route');
+dotenv.config();
 
-//INITIALIZATION
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
-//ROUTES AND MIDDLEWARES
-app.get('/', async (req, res, next) => {
-  res.send({ message: 'Awesome it works. Happy Coding🚀' });
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  res.send({ message: 'Awesome it works with TypeScript 🐻' });
 });
 
-app.use('/api', apiDocsPage);
+app.use('/api', apiRouter);
 
-//ERROR HANDLING ROUTE
-app.use(errorRoute);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError.NotFound());
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500);
+  res.send({
+    status: err.status || 500,
+    message: err.message,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+
+export default app; 
